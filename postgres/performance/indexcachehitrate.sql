beb4751deceb1264f8da7dd638f11cc240c8c2b8
rob sullivan <rob@datachomp.com>
2012-12-09 20:56:01 -0600

    postgres index cache hit rate

---
+++ postgres/performance/indexcachehitrate.sql
@@ -0,0 +1,8 @@
+#http://www.craigkerstiens.com/2012/10/01/understanding-postgres-performance/
+#You want around 99%
+SELECT 
+  sum(idx_blks_read) as idx_read,
+  sum(idx_blks_hit)  as idx_hit,
+  (sum(idx_blks_hit) - sum(idx_blks_read)) / sum(idx_blks_hit) as ratio
+FROM 
+  pg_statio_user_indexes;
